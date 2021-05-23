e5fcb30b2705ab99a3dacd4173a989eac0cebeb1
Yosua Krisnando Bagaskara <yosua1010101@gmail.com>
2021-05-23 21:24:34 +0700

    Created Form Section
    Created the form section with the button that is only enabled in the presence of a pdf file.

---
+++ frontend-web/src/component/input-form/FormPage.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+export default function FormPage() {
+    const [file, setFile] = useState("")
+
+    const handleFileChange = () => {
+        setFile(document.querySelector('#fileInput').value)
+    }
+    function useAxios(params) {
+        //TODO : Implement Axios to send pdf file to backend
+    }
+
+    return (
+        <form className="" onSubmit={useAxios()}>
+            <div className="d-grid gap-3 col-2 mx-auto">
+                <input type="file" id="fileInput" accept=".pdf,application/pdf" onChange={handleFileChange}/>
+                <br></br>
+                <button id="submitButton" className="btn btn-secondary" type="submit" disabled={!file}>Submit</button>
+            </div>
+        </form>
+    )
+}
