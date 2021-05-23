58c0306857feb0ed511eb50813d6ef9fd2910c43
Yosua Krisnando Bagaskara <yosua1010101@gmail.com>
2021-05-23 21:43:12 +0700

    Created Result section

--- frontend-web/src/component/result-section/ResultPage.js
+++ frontend-web/src/component/result-section/ResultPage.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
+const SummarySuggestion = () => {
+    //TODO: Make the summary suggestion block
+}
 
-export default function ResultPage() {
-    
+export default function ResultPage(quantity) {
+    var summarySuggestions = []
+    for (var index=0 ; index < quantity ; index++){
+        summarySuggestions.push(<SummarySuggestion/>)
+    }
+    return summarySuggestions;
 }
