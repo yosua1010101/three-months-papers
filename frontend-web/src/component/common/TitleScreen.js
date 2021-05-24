449b71f58584742b2880a996fd38de5e6954ed97
Yosua Krisnando Bagaskara <yosua1010101@gmail.com>
2021-05-25 00:15:27 +0700

    Changed from SUMMARIZ-IT to SUMMARIZ-it

--- frontend-web/src/component/common/TitleScreen.js
+++ frontend-web/src/component/common/TitleScreen.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function TitleScreen(){
     return(
         <div id="titlename" className="text-center">
-            <h1>SUMMARIZ-IT</h1>
+            <h1>SUMMARIZ-it</h1>
             <h3>by Three Months Papers</h3>
         </div>
         );
