92d96f7005cc7995d2b7f52bbe5553cea7d8699e
Yosua Krisnando Bagaskara <yosua1010101@gmail.com>
2021-05-29 14:25:26 +0700

    Restructured form section

--- frontend-web/src/component/input-form/FormPage.js
+++ frontend-web/src/component/input-form/FormPage.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import {useDropzone} from 'react-dropzone';
 import {TextareaAutosize} from '@material-ui/core';
 
-function FormPage(props) {
+export default function FormPage({handleSectionChange}) {
   const [files, setFiles] = useState([]);
-  
+
   const {getRootProps, getInputProps, open} = useDropzone({
-    accept: ".pdf,application/pdf",
+    accept: "application/pdf",
     onDrop: (acceptedFiles) => {
       setFiles(
         acceptedFiles.map((file) => Object.assign(file, {
@@ -16,6 +16,8 @@ function FormPage(props) {
     },
     noClick: true,
     noKeyboard: true,
+    multiple: false,
+    onDropRejected: ()=>{alert("File is not a PDF")},
   });
 
   const fileInput = files.map((file) =>
@@ -24,31 +26,26 @@ function FormPage(props) {
       <div>
         {file.name} - {file.size}
         {console.log(files)}
-        {/* <img src={file.preview} style={{ width: "200px" }} alt="preview"/> */}
       </div>
     </div>
     )
   )
 
   return(
-    <form className="container" onSubmit={props.handleSectionChange}>
+    <form className="container" onSubmit={handleSectionChange()}>
       <div className="box">
         <div {...getRootProps()} className="box-content">
           <input {...getInputProps()} />
-          <TextareaAutosize aria-label="minimum height" rowsMin={20} placeholder="Drag and Drop Here" className="textarea"/>
-          <div>{fileInput}</div>
+          <TextareaAutosize aria-label="minimum height" rowsMin={20} placeholder="Drag and Drop an Article here" className="textarea"/>
           <div className="sticky-btn">
             <button className="btn btn-success" type="button" onClick={open}>Open File Dialog</button>
+            <div className='py-2'/>
             <button id="submitButton" className="btn btn-secondary" type="submit" disabled={!files[0]}>Submit</button>
           </div>
         </div>
-        <div className="divider"></div>
-        <div className="box-content">
-          <TextareaAutosize aria-label="minimum height" rowsMin={20} placeholder="Output" className="textarea"/>
-        </div>
+        <div className="divider"/>
+        <div>{fileInput}</div>
       </div>
     </form>
   );
 }
-
-export default FormPage;
